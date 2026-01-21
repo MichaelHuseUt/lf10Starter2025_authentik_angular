27df4bf1488c38d7580173e8f1aff405aff46903
Nico <egni.hb@gmail.com>
2026-01-21 15:04:43 +0100

    -add addEmployee logic

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,6 +1,8 @@
 import {Component, ViewChild, ElementRef, output, Output, EventEmitter} from '@angular/core';
 import {Employee} from "../Employee";
 import {Observable, of} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "../../service/auth/auth.service";
 
 @Component({
   selector: 'app-add-employee',
@@ -9,18 +11,22 @@ import {Observable, of} from "rxjs";
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
-  employees$: Observable<Employee[]>;
 
-  constructor() {
-    this.employees$ = of([]);
+  constructor(private http: HttpClient, private authService: AuthService) {
   }
 
   @Output() closePopup: EventEmitter<void> = new EventEmitter();
 
-  addEmployee(firstname: string, lastname: string, street: string,
-              postcode: string, city: string, phoneNumber: string): void {
+  addEmployee(firstName: string, lastName: string, street: string,
+              postcode: string, city: string, phone: string): void {
 
+    const token = this.authService.getAccessToken();
 
-
+    this.http.post<Employee>('http://localhost:8089/employees',
+      {firstName, lastName, street, postcode, city, phone, skillSet: []},
+      {headers: new HttpHeaders()
+          .set('Content-Type', 'application/json')
+          .set('Authorization', `Bearer ${token}`)
+      });
   }
 }
