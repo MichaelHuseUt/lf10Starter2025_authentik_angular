f3d54805cd03f7300609f20943bad0ad555cfe19
Nico <egni.hb@gmail.com>
2026-01-21 11:17:43 +0100

    -add Button for addEmployee Method and merge-changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CallbackComponent } from './callback/callback.component';
 import { authGuard } from './auth.guard';
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -33,7 +33,6 @@ export class EmployeeListComponent {
 
   addEmployee(newEmployee: Employee): void {
 
-
   }
 
   fetchData() {
